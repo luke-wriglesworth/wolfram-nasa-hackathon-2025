5ba6eacfe7f418d90fbee94b71a5e1a09240f26e
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-10-04 15:29:22 -0400

    Add phytoplankton population data

---
+++ data_fetcher/main.py
@@ -0,0 +1,25 @@
+import earthaccess
+import dotenv
+from loguru import logger
+dotenv.load_dotenv()
+
+
+def auth():
+    earthaccess.login(strategy="environment")
+    logger.info(f"Auth Status: {earthaccess.status()}")
+
+def main():
+    auth()
+    # realtime phytoplankton populations
+    near_realtime_phytoplankton_search = earthaccess.search_data(
+        short_name='PACE_OCI_L4M_MOANA_NRT',
+        # bounding_box=(-10, 20, 10, 50),
+        temporal=("2024-01", "2025-10"),
+        count=-1
+    )
+    logger.info(f"Found {len(near_realtime_phytoplankton_search)} phytoplankton results")
+    earthaccess.download(near_realtime_phytoplankton_search, "./realtime_phytoplankton_populations")
+
+
+if __name__ == "__main__":
+    main()
