d01ff922888b9e27444b659e612ffc4b0de95b72
Luke Wriglesworth <luke.wriglesworth@mail.utoronto.ca>
2025-10-04 15:47:13 -0400

    add NASA example notebooks

--- data_fetcher/main.py
+++ data_fetcher/main.py
@@ -11,15 +11,23 @@ def auth():
 def main():
     auth()
     # realtime phytoplankton populations
-    near_realtime_phytoplankton_search = earthaccess.search_data(
-        short_name='PACE_OCI_L4M_MOANA_NRT',
+    # near_realtime_phytoplankton_search = earthaccess.search_data(
+    #     short_name='PACE_OCI_L4M_MOANA_NRT',
+    #     # bounding_box=(-10, 20, 10, 50),
+    #     temporal=("2024-01", "2025-10"),
+    #     count=-1
+    # )
+    # logger.info(f"Found {len(near_realtime_phytoplankton_search)} phytoplankton results")
+    # earthaccess.download(near_realtime_phytoplankton_search, "./realtime_phytoplankton_populations")
+
+    chlorophyll_concentration = earthaccess.search_data(
+        short_name='PACE_OCI_L3B_CHL',
         # bounding_box=(-10, 20, 10, 50),
-        temporal=("2024-01", "2025-10"),
+        temporal=("2024-03-05", "2024-03-12"),
         count=-1
     )
-    logger.info(f"Found {len(near_realtime_phytoplankton_search)} phytoplankton results")
-    earthaccess.download(near_realtime_phytoplankton_search, "./realtime_phytoplankton_populations")
-
+    logger.info(f"Found {len(chlorophyll_concentration)} chlorophyll results")
+    earthaccess.download(chlorophyll_concentration, "./chlorophyll_data")
 
 if __name__ == "__main__":
     main()
---
+++ readme.md
@@ -0,0 +1,2 @@
+### Other notebook examples from NASA not in this repo
+https://pacehackweek.github.io/pace-2025/presentations/notebooks.html
